91adf8af1d5386cd83ccb982e6a2970cf5cfe958
will.doan <info.weTheBest@gmail.com>
2022-10-23 13:09:20 -0700

    important for tailwind

--- pages/checkout/review.js
+++ pages/checkout/review.js
@@ -16,7 +16,6 @@ export default function Checkout() {
 
   let checkout = useCheckoutCreate();
   // Update the line incase user comeback and add more to cart
-  // let checkoutUpdate = useCheckoutUpdateLines()
 
   // Create checkout when first enter only
   useEffect(() => {
