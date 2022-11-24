0a63cbaff12872e807aebfedc9e6ce29fe00d09a
will.doan <info.weTheBest@gmail.com>
2022-11-23 19:24:44 -0800

    Update payment gateway

--- components/Stripe/CheckoutForm.js
+++ components/Stripe/CheckoutForm.js
@@ -1,50 +1,60 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import {
-    CardElement,
-    CardCvcElement,
-    CardExpiryElement,
-    useStripe,
-    useElements,
-} from '@stripe/react-stripe-js';
+  CardElement,
+  CardCvcElement,
+  CardExpiryElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import Button from "@mui/material/Button";
 
 export default function CheckoutForm() {
-    const stripe = useStripe();
-    const elements = useElements();
+  const stripe = useStripe();
+  const elements = useElements();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        if (elements == null) {
-            return;
-        }
+    if (elements == null) {
+      return;
+    }
 
-        // const { error, paymentMethod } = await stripe.createPaymentMethod({
-        //     type: 'card',
-        //     card: elements.getElement(CardElement),
-        // });
-        // console.log(paymentMethod)
-        // let token = await stripe.createToken(elements.getElement(CardElement))
-        // console.log(token)
-        // const { paymentIntent, error } = stripe.confirmCardPayment("pi_3M7T29DLvTNEyZ8917d8HhId_secret_J7pzvUnsSrlpsaA8HUYE7AnWH", {
-        //     payment_method: {
-        //         card: elements.getElement(CardElement),
-        //         billing_details: {
-        //             name: "Nam Doan"
-        //         }
-        //     }
-        // })
-        // console.log(paymentIntent, error)
+    // const { error, paymentMethod } = await stripe.createPaymentMethod({
+    //     type: 'card',
+    //     card: elements.getElement(CardElement),
+    // });
+    // console.log(paymentMethod)
+    // let token = await stripe.createToken(elements.getElement(CardElement))
+    // console.log(token)
+    // const { paymentIntent, error } = stripe.confirmCardPayment("pi_3M7T29DLvTNEyZ8917d8HhId_secret_J7pzvUnsSrlpsaA8HUYE7AnWH", {
+    //     payment_method: {
+    //         card: elements.getElement(CardElement),
+    //         billing_details: {
+    //             name: "Nam Doan"
+    //         }
+    //     }
+    // })
+    // console.log(paymentIntent, error)
+  };
 
-    };
+  useEffect(() => {}, []);
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <CardElement className='px-5 py-5 bg-slate-100' />
-            <button type="submit" disabled={!stripe || !elements}>
-                Pay
-            </button>
-        </form>
-    )
+  return (
+    <form className="mt-20 mx-20" onSubmit={handleSubmit}>
+      <div className="relative px-5 py-5 w-96 flex flex-col space-y-3 justify-center items-center bg-slate-100">
+        <CardElement className="px-5 py-5 w-full" />
+        <Button
+          variant="outlined"
+          type="submit"
+          size="small"
+          disabled={!stripe || !elements}
+          className="text-white normal-case bg-black border-black rounded-full w-44 hover:bg-white hover:text-black"
+        >
+          Pay Now
+        </Button>
+      </div>
+    </form>
+  );
 }
 // tok_1M7ShnDLvTNEyZ89o2cuDTp1
 // pm_1M7ShnDLvTNEyZ89WDtsCJRD
--- pages/checkout/payment/[index].js
+++ pages/checkout/payment/[index].js
@@ -1,84 +1,104 @@
-import React, { useState, useEffect } from 'react'
-import { QueryClient, dehydrate, useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import React, { useState, useEffect } from "react";
+import { QueryClient, dehydrate, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-import { decryptText, setCookie, getCookie } from '../../../utils/utils';
-import { checkoutGet } from '../../../lib/serverRequest';
-// import CheckoutForm from '../'
+import { decryptText, setCookie, getCookie } from "../../../utils/utils";
+import { checkoutGet } from "../../../lib/serverRequest";
+import CheckoutForm from "../../../components/Stripe/CheckoutForm";
+import Loading from "../../../components/Loading/dataLoading";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
 
 // This page need checkoutId
-export default function Index() {
-    let [isCreateIntent, setCreateIntent] = useState(false)
-    let [stripeOption, setStripeOption] = useState({clientSecret: "pi_3M7UMTDLvTNEyZ890lBMF7xl_secret_OZvOGEZmIA4UXjrFXK7rJM3Fl"}) //pi_3M7UMTDLvTNEyZ890lBMF7xl_secret_OZvOGEZmIA4UXjrFXK7rJM3Fl
+export default function Index({ id }) {
+  let [isCreateIntent, setCreateIntent] = useState(false);
+  let [stripeOption, setStripeOption] = useState({
+    clientSecret:
+      "pi_3M7UMTDLvTNEyZ890lBMF7xl_secret_OZvOGEZmIA4UXjrFXK7rJM3Fl",
+  }); //pi_3M7UMTDLvTNEyZ890lBMF7xl_secret_OZvOGEZmIA4UXjrFXK7rJM3Fl
 
-    let { data } = useQuery(
-        ["checkout", "allo-ultra-2500-disposable"],
-        () => checkoutGet({ id: decryptText("U2FsdGVkX18hUWkOiDfkRKIT7nmAs4u6BbQAi/NVubU6UagHvC4IJlObnKBL8xN7qSN3/ej5OKBGHdJ1/uP5C2aQZe3St/369zdHNoEMTRU9VRvu6c/D1s8kdCZNQ5yffwpMoC3E1dqHsRfQ3Wwf2w==") }),
-        { staleTime: 1000 * 60 * 60 * 24 }
-    )
+  let { data } = useQuery(
+    ["checkout", id],
+    () =>
+      checkoutGet({
+        id: id,
+      }),
+    { staleTime: 1000 * 60 * 60 * 24 }
+  );
+  console.log(data);
 
-    let createPaymentIntent = async (total, currency, method) => {
-        let data = await axios.post("/api/stripe/payment-intent-create", {
-            data: {
-                amount: total * 100,
-                currency: currency,
-                payment_method_types: method
-            }
-        })
-        setCookie("pi", JSON.stringify(data))
-        setStripeOption({ clientSecret: data.data})
-        setCreateIntent(true)
-        return data
+  let createPaymentIntent = async (total, currency, method) => {
+    let data = await axios.post("/api/stripe/payment-intent-create", {
+      data: {
+        amount: total * 100,
+        currency: currency,
+        payment_method_types: method,
+      },
+    });
+    setCookie("pi", JSON.stringify(data));
+    setStripeOption({ clientSecret: data.data });
+    setCreateIntent(true);
+    return data;
+  };
+
+  useEffect(() => {
+    let pi = getCookie("pi");
+    if (!pi) {
+      let dataNode = data.data.node;
+      let total = parseFloat(dataNode.totalPriceV2.amount);
+      let currency = "cad"; // Default
+      let method = "card"; // Default
+
+      //   let paymentIntent = createPaymentIntent(total, currency, method);
+      // console.log(paymentIntent)
+    } else {
+      setCreateIntent(true);
     }
+  }, []);
 
-    useEffect(() => {
-        let pi = getCookie("pi")
-        if (!pi) {
-            let dataNode = data.data.node
-            let total = parseFloat(dataNode.totalPriceV2.amount)
-            let currency = "cad" // Default
-            let method = "card" // Default
-    
-            // let paymentIntent = createPaymentIntent(total, currency, method)
-            // console.log(paymentIntent)
-        }
-    }, [])
-
-    if (!isCreateIntent) return (
-        <>
-            <p>Loading</p>
-        </>
-    )
-
+  if (!isCreateIntent)
     return (
-        <>
-            <Elements stripe={stripePromise} options={stripeOption}>
-            </Elements>
-        </>
-    )
-}
+      <>
+        <div className="absolute top-1/3 left-1/2">
+          <Loading />
+        </div>
+      </>
+    );
 
+  return (
+    <>
+      <Elements stripe={stripePromise} options={stripeOption}>
+        <CheckoutForm />
+      </Elements>
+    </>
+  );
+}
 
 const queryClient = new QueryClient();
 export async function getServerSideProps({ query }) {
-    let { index } = query;
-    await queryClient.prefetchQuery(
-        ["checkout", "allo-ultra-2500-disposable"],
-        () => checkoutGet({ id: decryptText("U2FsdGVkX18hUWkOiDfkRKIT7nmAs4u6BbQAi/NVubU6UagHvC4IJlObnKBL8xN7qSN3/ej5OKBGHdJ1/uP5C2aQZe3St/369zdHNoEMTRU9VRvu6c/D1s8kdCZNQ5yffwpMoC3E1dqHsRfQ3Wwf2w==") }),
-        {
-            staleTime: 1000 * 60 * 60 * 24,
-        }
-    );
+  let { index } = query;
+  index = decodeURIComponent(decryptText(index));
+  console.log(index);
+  await queryClient.prefetchQuery(
+    ["checkout", index],
+    () =>
+      checkoutGet({
+        id: index,
+      }),
+    {
+      staleTime: 1000 * 60 * 60 * 24,
+    }
+  );
 
-
-    return {
-        props: {
-            dehydratedState: dehydrate(queryClient),
-        },
-    };
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      id: index,
+    },
+  };
 }
 // U2FsdGVkX18hUWkOiDfkRKIT7nmAs4u6BbQAi/NVubU6UagHvC4IJlObnKBL8xN7qSN3/ej5OKBGHdJ1/uP5C2aQZe3St/369zdHNoEMTRU9VRvu6c/D1s8kdCZNQ5yffwpMoC3E1dqHsRfQ3Wwf2w==
